0fd441ecd7cc81881e18866486f6bedaa149f604
duponto <gabriel.327m@gmail.com>
2024-01-02 01:09:00 -0300

    feat: barra de icones

--- src/components/StackIconsRow/index.tsx
+++ src/components/StackIconsRow/index.tsx
@@ -1,14 +1,19 @@
+import styles from './StackIconsRow.module.css';
 import cSharpSvg from '../../assets/icons/cs.svg';
 import netCoreSvg from '../../assets/icons/NET-core.svg';
 import jsSvg from '../../assets/icons/js.svg';
 import reactSvg from '../../assets/icons/react.svg';
 import tsSvg from '../../assets/icons/ts.svg';
 import jQuerySvg from '../../assets/icons/jquery.svg';
-import styles from './StackIconsRow.module.css';
+import postgreSqlSvg from '../../assets/icons/postgresql.svg';
+import gitSvg from '../../assets/icons/git.svg';
+// import vscSvg from '../../assets/icons/vsc.svg';
+// import vsSvg from '../../assets/icons/visual-studio.svg';
 
 
 export default function StackIconsRow() {
     return (
+        <>
         <div className={styles.container}>
             <img src={cSharpSvg} alt="CSharp logo" title="CSharp logo" />
             <img src={netCoreSvg} alt=".NET Core logo" title=".NET Core logo" />
@@ -16,6 +21,9 @@ export default function StackIconsRow() {
             <img src={jQuerySvg} alt="jQuery logo" title="jQuery logo" />
             <img src={reactSvg} alt="React.js logo" title="React logo" />
             <img src={tsSvg} alt="Typescript logo" title="Typescript logo" />
+            <img src={postgreSqlSvg} alt="PostgreSQL logo" title="PostgreSQL logo" />
+            <img src={gitSvg} alt="Git logo" title="Git logo" />
         </div>
+        </>
     )
 }
