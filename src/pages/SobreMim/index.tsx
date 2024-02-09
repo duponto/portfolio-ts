12558f0e5b8ff6866a45c40793690d680afc9c37
duponto <gabriel.327m@gmail.com>
2024-02-09 16:05:24 -0300

    changes no texto sobre mim

--- src/pages/SobreMim/index.tsx
+++ src/pages/SobreMim/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./SobreMim.module.css";
 import PostModelo from "components/PostModelo";
 
@@ -21,8 +22,16 @@ export default function SobreMim() {
                 sempre tento deixar o código o mais limpo e legível possível. 
                 Também gosto de aprender novas tecnologias e ferramentas, 
                 e de aplicar o que aprendi em projetos pessoais. 
-                Acredito que nem sempre se pode dizer que aprendeu algo apenas assistindo, 
+                Acredito que nem sempre se pode dizer que aprendeu algo apenas assistindo/lendo/ouvindo, 
                 é preciso por a mão na massa e apanhar um pouquinho no aprendizado.
+            </p>
+            <p className={styles.paragrafo}>
+                Minha carreira começou em 2021, sendo estagiário de desenvolvimento na{" "}
+                <Link to={"https://www.saur.com.br/"} target="_blank" className={styles.link}>
+                    SAUR Equipamentos S/A
+                </Link>
+                {" "}Onde tive meu primeiro contato profissional com linguagens de programação,
+                lidando principalmente com HTML, CSS e JavaScript.
             </p>
         </PostModelo>
     )
