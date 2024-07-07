48d6332b9245df8a8e2a847d513278433b38ee12
Gabriel Dupont <gabriel.327m@gmail.com>
2024-07-07 17:14:17 -0300

    feat: icones no rodape + i fontawesome

--- src/components/Rodape/index.tsx
+++ src/components/Rodape/index.tsx
@@ -1,9 +1,20 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import styles from './Rodape.module.css'
+import { Link } from 'react-router-dom';
 
 export default function Rodape() {
     return (
-        <footer className={styles.rodape}>
+        <div className={styles.footer}>
             Desenvolvido por Gabriel
-        </footer>
+            <div className={styles.icons}>
+                <Link to={'https://github.com/duponto'} target='_blank'>
+                    <FontAwesomeIcon icon={faGithub} size='xl'/>
+                </Link>
+                <Link to={'https://www.linkedin.com/in/gabrielmuenchen/'} target='_blank'>
+                    <FontAwesomeIcon icon={faLinkedin} size='xl'/>
+                </Link>
+            </div>
+        </div>
     )
 }
